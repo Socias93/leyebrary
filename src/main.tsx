3c8a7c0388a4bbb073ded4fbb4b7c527146c84f8
Christian Socias <Socias_93@hotmail.com>
2025-11-18 17:59:34 +0100

    added router and importet routerprovider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
