1fc83f448dfafbaf55dc3049c8c719bb4202ea28
Christian Socias <Socias_93@hotmail.com>
2025-11-19 18:25:07 +0100

    created delete function for the items

--- src/pages/AllItemsPage.tsx
+++ src/pages/AllItemsPage.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import { deleteItem, getItems } from "../services/fakeFoodService";
+
 function AllItemsPage() {
-  return <h1>Items</h1>;
+  const [items, setItems] = useState(getItems());
+
+  function handleDelete(id: string) {
+    const newItem = items.filter((item) => item._id !== id);
+    setItems(newItem);
+    deleteItem(id);
+  }
+
+  return (
+    <>
+      <table className="table">
+        <thead className="table-info">
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Category</th>
+            <th />
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr key={item._id}>
+              <td>{item.title} </td>
+              <td>{item.category.name} </td>
+              <td>
+                <button className="btn btn-outline-info">Edit</button>
+              </td>
+              <td>
+                <button
+                  onClick={() => handleDelete(item._id)}
+                  className="btn btn-outline-dark">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default AllItemsPage;
