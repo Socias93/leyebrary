4f370669ab8f4cfa01acfda6559aea201f14ba57
Christian Socias <Socias_93@hotmail.com>
2025-11-21 18:08:09 +0100

    created basic delete functionality for categories

--- src/pages/categories/AllCategoriesPage.tsx
+++ src/pages/categories/AllCategoriesPage.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { getCategories } from "../../services/fakeCategoryService";
 
 function AllCategoriesPage() {
-  const categories = getCategories();
+  const [categories, setCategories] = useState(getCategories());
+
+  function handleDelete(id: string) {
+    const newCategory = categories.filter((category) => category._id !== id);
+
+    setCategories(newCategory);
+  }
 
   return (
     <>
@@ -10,17 +17,21 @@ function AllCategoriesPage() {
           <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-3">
             {categories.map((category) => (
               <div key={category._id} className="col">
-                <div className="card h-100 shadow-sm border-0 rounded-4 relative">
+                <div className="card h-100 shadow-lg border-0 rounded-4 relative">
                   <img
                     src={category.imageUrl}
                     alt={category.name}
                     className="card-img-top rounded-top-4 relative"
                     style={{ height: 160, objectFit: "cover" }}
                   />
-                  <div className="card-body relative">
-                    <div className="d-flex align-items-start justify-content-between">
-                      <h5 className="card-title mb-1">{category.name}</h5>
-                    </div>
+                  <div className="d-grid justify-content-center">
+                    <h3 className="text-center">{category.name}</h3>
+                    <span
+                      onClick={() => handleDelete(category._id)}
+                      className="clickable badge bg-dark rounded-pill p-2 shadow mt-2 mb-3"
+                      style={{ width: 150 }}>
+                      Delete
+                    </span>
                   </div>
                 </div>
               </div>
