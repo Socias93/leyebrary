fafbf365ee1cbb7856a8b22e2c4f8689e52fc534
Christian Socias <Socias_93@hotmail.com>
2025-11-22 18:32:05 +0100

    fixed unique key issue in tablebody

--- src/components/TableBody.tsx
+++ src/components/TableBody.tsx
@@ -12,13 +12,14 @@ function TableBody({ columns, items }: Props) {
     <tbody>
       {items.map((item) => (
         <tr key={item._id}>
-          {columns.map((column) =>
-            "content" in column ? (
-              <td key={column.key}> {column.content(item)} </td>
+          {columns.map((column) => {
+            const cellKey = "key" in column ? column.key : column.path;
+            return "content" in column ? (
+              <td key={`${item._id}-${cellKey}`}>{column.content(item)}</td>
             ) : (
-              <td key={column.path}>{_.get(item, column.path)} </td>
-            )
-          )}
+              <td key={`${item._id}-${cellKey}`}>{_.get(item, column.path)}</td>
+            );
+          })}
         </tr>
       ))}
     </tbody>
