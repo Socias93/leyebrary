645e34e295039d6d05e4dba1dfdc6687205c51e7
Christian Socias <Socias_93@hotmail.com>
2025-11-25 16:03:56 +0100

    made listgroup responsive

--- src/components/Listgroup.tsx
+++ src/components/Listgroup.tsx
@@ -8,18 +8,18 @@ interface Props {
 
 function Listgroup({ items, onCategorySelect, selectedCategory }: Props) {
   return (
-    <>
+    <div className="d-flex flex-wrap">
       {items.map((item) => (
         <button
           onClick={() => onCategorySelect(item)}
           key={item.id}
-          className={`mt-2 btn btn-outline-info rounded-4 m-1 ${
+          className={`mt-1 mt-sm-2 btn btn-sm btn-outline-info rounded-4 m-1 px-2 px-sm-3 ${
             item.id === selectedCategory.id ? "active" : ""
-          }   `}>
+          }`}>
           {item.name}
         </button>
       ))}
-    </>
+    </div>
   );
 }
 
