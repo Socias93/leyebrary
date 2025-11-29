3baa97782d5b64cad81c81775de3b8f3702bf47c
Christian Socias <Socias_93@hotmail.com>
2025-11-30 00:34:04 +0100

    updated url in env

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { Navbar } from "./components/index";
 
 function App() {
+  console.log("API URL:", import.meta.env.VITE_API_URL);
+
   return (
     <>
       <Navbar />
