c0df816b66f3b06c994adbbeee90241a6f11ba25
Christian Socias <Socias_93@hotmail.com>
2025-11-30 00:57:43 +0100

    updated env url again

--- src/pages/categories/categoryschema/CreateCategorySchema.ts
+++ src/pages/categories/categoryschema/CreateCategorySchema.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const categorySchema = z.object({
   name: z.string().min(1, "Category name is required"),
-
   fields: z
     .array(z.enum(["author", "nbrPages", "runTimeMinutes"]))
     .min(1, { message: "You must choose at least 1 field" }),
